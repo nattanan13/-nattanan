b5cd0d85cdf80f830618712a9ff8c5dd358ca856
TAMAMABABO\Acer <nattanan.prompanya@gmail.com>
2021-07-28 00:01:28 +0700

    หยิบกี่อันก็ได้แล้ว แต่ไม่เกินmax_take

--- 640631119__Nattanan_sticks vs AI.py
+++ 640631119__Nattanan_sticks vs AI.py
@@ -2,7 +2,7 @@ print("The Stick in the Pile Game")
 player2 = str(input("What's your name? : "))
 sticks = int(input("How many sticks in the pile? : "))
 Max_take = int(input("The maximum amount that can be took: "))
-
+import random
 while sticks > 0 :
   print("There are " + str(sticks) + " sticks in the pile ")
 
@@ -16,9 +16,12 @@ while sticks > 0 :
     take1 = random.randint(1, Max_take)
     sticks -= take1
     print("I take " + str(take1) + " stick, there are " + str(sticks) + " sticks in the pile. ")
-
+  if sticks <= 0:
+    print(" AI takes the last stick,")
+    print(player2," WON ")
+    break
 #player2 (people)
-  
+
   take2 = int(input("How many you will take: "))
   if take2 <= Max_take:
     sticks -= take2
@@ -26,7 +29,6 @@ while sticks > 0 :
     print("No! you can't take more less than 1 stick!")
     take2 = int(input("How many you will take: "))
     sticks -= take2
-
   if sticks== 0:
     print(player2, "takes the last stick,")
     print("I WON (Python won) ")
