f3baf125115205843a5448c82960f96639ad03ed
TAMAMABABO\Acer <nattanan.prompanya@gmail.com>
2021-07-27 13:26:56 +0700

    no message

---
+++ The largest formed.py
@@ -0,0 +1,11 @@
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+c = input('Enter the third number: ')
+d = input('Enter the last number: ')
+
+L = [a,b,c,d]
+
+for i in L :
+  print(i)
+
+
